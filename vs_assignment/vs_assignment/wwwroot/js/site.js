b2529047be1607b8bcd7449d3a5fb2dff4866000
MiniFid <85532592+minifid@users.noreply.github.com>
2022-05-23 12:32:59 -0600

    Add files via upload

---
+++ vs_assignment/vs_assignment/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+var id;
+
+function animation() {
+    var vid = document.getElementById("video");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            vid.style.top = pos + 'px';
+            vid.style.left = pos + 'px';
+        }
+    }
+}
+
+animation()
